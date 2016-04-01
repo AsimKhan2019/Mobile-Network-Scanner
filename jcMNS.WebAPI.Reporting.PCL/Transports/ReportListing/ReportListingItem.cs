db3857ac6b6c9701c6bdd667c2d981a07e260f48
Jarred Capellman <jcapellman@hotmail.com>
2016-04-01 16:19:04 -0700

    Initial commit of the Reporting PCL

---
+++ jcMNS.WebAPI.Reporting.PCL/Transports/ReportListing/ReportListingItem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace jcMNS.WebAPI.Reporting.PCL.Transports.ReportListing {
+    public class ReportListingItem {
+        public Guid ReportGUID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
