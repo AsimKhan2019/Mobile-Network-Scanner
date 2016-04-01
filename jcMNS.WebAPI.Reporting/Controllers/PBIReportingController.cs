b68768d30b72b8caa46426b36f0da6419dc242b6
Jarred Capellman <jcapellman@hotmail.com>
2016-04-01 09:17:48 -0700

    Added Initial commit of the Reporting WebAPI Service

---
+++ jcMNS.WebAPI.Reporting/Controllers/PBIReportingController.cs
@@ -0,0 +1,14 @@
+﻿using jcMNS.Library.WebAPI;
+
+using Microsoft.AspNet.Mvc;
+
+namespace jcMNS.WebAPI.Reporting.Controllers {
+    public class PBIReportingController : BaseController
+    {
+        [HttpGet]
+        public string GET()
+        {
+            return "test";
+        }
+    }
+}
