9a7e434501526fb1912435ce10dbff4fac6ac5dd
Jarred Capellman <jcapellman@hotmail.com>
2016-04-01 19:37:58 -0700

    Added Reporting Library project

---
+++ jcMNS.WebAPI.Reporting.Library/Transports/ReportListing/ReportListingItem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace jcMNS.WebAPI.Reporting.Library.Transports.ReportListing {
+    public class ReportListingItem {
+        public Guid ReportGUID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
