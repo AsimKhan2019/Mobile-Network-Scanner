ce4c9df81dce1fc6a3e37dcd4d3ca42c15f6c29c
Jarred Capellman <jcapellman@hotmail.com>
2016-04-03 07:26:10 -0700

    Initial commit of the Ping Back Data Layer

---
+++ jcMNS.WebAPI.Pingback.DataLayer/EntityObjects/PingBacks/jcMNS_PingBacks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace jcMNS.WebAPI.Pingback.DataLayer.EntityObjects.PingBacks {
+    public class jcMNS_PingBacks {
+        public int ID { get; set; }
+
+        public DateTimeOffset Created { get; set; }
+
+        public DateTimeOffset Modified { get; set; }
+
+        public bool Active { get; set; }
+
+        public Guid DeviceGUID { get; set; }
+
+        public string Errors { get; set; }
+    }
+}
