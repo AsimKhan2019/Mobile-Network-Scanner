17c69a1e09ec5d7307070dba8492bd08dd90720a
jcapellman <jcapellman@hotmail.com>
2016-04-04 20:34:20 -0400

    Started implementing the PingBackManager class

---
+++ jcMNS.WebAPI.Pingback/Managers/PingBackManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using jcMNS.Library.WebAPI;
+using jcMNS.WebAPI.Pingback.DataLayer.Entities;
+using jcMNS.WebAPI.Pingback.DataLayer.EntityObjects.PingBacks;
+using jcMNS.WebAPI.Pingback.Library.Transports;
+
+namespace jcMNS.WebAPI.Pingback.Managers {
+    public class PingBackManager : BaseManager {
+        public bool CreatePingBack(PingBackRequestItem requestItem) {
+            using (var eFactory = new PingBackDBFactory()) {
+                var nRow = new jcMNS_PingBacks();
+
+                nRow.Active = true;
+                nRow.Created = DateTimeOffset.Now;
+                nRow.Modified = DateTimeOffset.Now;
+                nRow.DeviceGUID = requestItem.DeviceGUID;
+                nRow.Errors = requestItem.Telemetry;
+
+                eFactory.PingBacks.Add(nRow);
+
+                eFactory.SaveChanges();
+
+                return true;
+            }
+        }
+    }
+}
