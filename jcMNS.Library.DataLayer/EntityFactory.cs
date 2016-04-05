0452cea47f58a1e17263e8c4f44517efcdc1809b
jcapellman <jcapellman@hotmail.com>
2016-04-04 20:33:37 -0400

    Added Base Data Layer Library

---
+++ jcMNS.Library.DataLayer/EntityFactory.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.Data.Entity;
+
+namespace jcMNS.Library.DataLayer {
+    public class EntityFactory : DbContext { }
+}
---
+++ jcMNS.Library.DataLayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("jcMNS.Library.DataLayer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("jcMNS.Library.DataLayer")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3b08b2e2-635f-4f4d-a5f2-e0d780f6c368")]
