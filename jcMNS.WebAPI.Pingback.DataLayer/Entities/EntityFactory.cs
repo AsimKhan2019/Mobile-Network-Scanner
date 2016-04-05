ee61b3a2ba9f28d96ffda90fcf684b4cfbe5328c
jcapellman <jcapellman@hotmail.com>
2016-04-04 20:33:21 -0400

    Started implementing the PingBackController

--- jcMNS.WebAPI.Pingback.DataLayer/Entities/EntityFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using jcMNS.WebAPI.Pingback.DataLayer.EntityObjects.PingBacks;
-
-using Microsoft.Data.Entity;
-
-namespace jcMNS.WebAPI.Pingback.DataLayer.Entities {
-    public class EntityFactory : DbContext {
-        public DbSet<jcMNS_PingBacks> PingBacks { get; set; } 
-    }
-}
--- jcMNS.WebAPI.Pingback/Controllers/PingbackController.cs
+++ jcMNS.WebAPI.Pingback/Controllers/PingbackController.cs
@@ -1,6 +1,12 @@
 ﻿using jcMNS.Library.WebAPI;
+using jcMNS.WebAPI.Pingback.Library.Transports;
+using jcMNS.WebAPI.Pingback.Managers;
+
+using Microsoft.AspNet.Mvc;
 
 namespace jcMNS.WebAPI.Pingback.Controllers {
     public class PingbackController : BaseController {
+        [HttpPost]
+        public bool CreatePingback(PingBackRequestItem requestItem) => new PingBackManager().CreatePingBack(requestItem);
     }
 }
