ea98d09bc3db63d2f440441986d70aa6d0cbcf75
Jarred Capellman <jcapellman@hotmail.com>
2016-04-08 22:50:51 -0400

    Continued work on Reporting Manager

---
+++ jcMNS.WebAPI.Reporting.DataLayer/EntityObjects/Reports/REPORTING_getDevicePingBackHistorySP.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace jcMNS.WebAPI.Reporting.DataLayer.EntityObjects.Reports {
+    public class REPORTING_getDevicePingBackHistorySP {
+        public DateTimeOffset Modified { get; set; }
+    }
+}
--- jcMNS.WebAPI.Reporting/Managers/ReportingManager.cs
+++ jcMNS.WebAPI.Reporting/Managers/ReportingManager.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;   
 using System.Linq;
 
 using jcMNS.Library.WebAPI;
 using jcMNS.WebAPI.Reporting.DataLayer.Entities;
 using jcMNS.WebAPI.Reporting.Library.Transports.ReportListing;
+using Microsoft.Data.Entity;
 
 namespace jcMNS.WebAPI.Reporting.Managers {
     public class ReportingManager : BaseManager {
@@ -15,5 +17,13 @@ namespace jcMNS.WebAPI.Reporting.Managers {
                 }).ToList();
             }
         }
+
+        public List<DateTimeOffset> REPORT_GetDevicePingBackHistory(Guid deviceGUID)
+        {
+            using (var eFactory = new ReportingDBFactory())
+            {
+                return new List<DateTimeOffset>();
+            }
+        } 
     }
 }
